ac99b80446cd92209d896b4bd609ff70e8aa138d
Immanuel <emmanuelmendoza448@gmail.com>
2026-02-14 13:48:41 -0600

    creación

---
+++ app/src/main/java/com/immanuel/groseriastranslatorv2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.immanuel.groseriastranslatorv2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.immanuel.groseriastranslatorv2.ui.theme.GroseriasTranslatorV2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GroseriasTranslatorV2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GroseriasTranslatorV2Theme {
+        Greeting("Android")
+    }
+}
